4580eae3e59dc82b73ba5ae2258d9c5cc7167ae4
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-14 04:06:50 -0400

    done

--- Glab-303.11.6/src/maicomparator.java
+++ Glab-303.11.6/src/maicomparator.java
@@ -1,2 +1,14 @@
+import java.util.TreeMap;
+
 public class maicomparator {
+    public static void main(String[] args) {
+        TreeMap <String, Integer> numbers = new TreeMap<>(new CustomComparator());
+
+        numbers.put("First", 1);
+        numbers.put("Second", 2);
+        numbers.put("Third", 3);
+        numbers.put("Fourth", 4);
+        System.out.println("TreeMap: "+numbers);
+
+    }
 }
